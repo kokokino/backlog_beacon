9a30f7a8fc3017084d2ad4c25e256ca3f8aeb990
Aaron Rosenzweig <info@kokokino.com>
2026-01-25 21:03:14 -0500

    Remove the "..." jiggle in the position indicator. It wasn't that useful and it was annoying to see it rapidly hide show the ellipsis.

--- imports/ui/components/PositionIndicator.js
+++ imports/ui/components/PositionIndicator.js
@@ -2,16 +2,15 @@ import m from 'mithril';
 
 export const PositionIndicator = {
   view(vnode) {
-    const { start, end, total, loading } = vnode.attrs;
+    const { start, end, total } = vnode.attrs;
     // console.log('[PositionIndicator] Rendering:', { start, end, total, loading });
 
     if (total === 0) {
       return null;
     }
 
-    return m('div.position-indicator', [
-      m('span', `${start.toLocaleString()}-${end.toLocaleString()} of ${total.toLocaleString()} games`),
-      loading && m('span.loading-dot', ' ...')
-    ]);
+    return m('div.position-indicator',
+      `${start.toLocaleString()}-${end.toLocaleString()} of ${total.toLocaleString()} games`
+    );
   }
 };
